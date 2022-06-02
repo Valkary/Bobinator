ff5e58172d37f17cff472c5300daf9826beb7c71
Valkary <pepoclesng@gmail.com>
2022-06-02 14:30:54 -0500

    Arreglando unos errores de typescript y eliminando @ts-ignore

--- src/components/organisms/OrdersLayout.tsx
+++ src/components/organisms/OrdersLayout.tsx
@@ -6,7 +6,7 @@ import { format } from 'date-fns';
 import { SocketContext } from '../../context/socket';
 
 import OrdersSkeleton from "../atoms/OrdersSkeleton";
-import OrderInProd from "../cells/OrderInProd";
+import ApproveOrderBtn from "../atoms/ApproveOrderBtn";
 import ProductionTimeline from "../atoms/ProductionTimeline";
 
 import useGetRequiredOrderLayoutData from "../../hooks/useGetRequiredOrderLayoutData";
@@ -15,11 +15,8 @@ const OrdersLayout: React.FunctionComponent<{ enoughWidth: boolean, prod_state: 
   const [selectedChip, setSelectedChip] = useState(['approved']);
 
   const requiredData = useGetRequiredOrderLayoutData();
-  
   const socket = useContext(SocketContext);
-  
   socket.emit('connected-from', 'OrdersLayout');
-  
   
   if (requiredData === "loading") {
     return <OrdersSkeleton />
@@ -37,6 +34,7 @@ const OrdersLayout: React.FunctionComponent<{ enoughWidth: boolean, prod_state: 
       });
 
       const etapas = orderStages;
+      console.log(etapas);
 
       const on_hold_orders = ordersOnHold;
       const finished_orders = finishedOrders;
@@ -45,21 +43,15 @@ const OrdersLayout: React.FunctionComponent<{ enoughWidth: boolean, prod_state: 
       
       return (
         <div className=" flex flex-col h-full w-full overflow-y-scroll scrollbar">
-          <Chips variant="filled" multiple onChange={setSelectedChip} value={selectedChip}>
-            <Chip value="approved">Aprobados</Chip>
-            <Chip value="finished">Terminados</Chip>
-            <Chip value="on-hold">En espera</Chip>
-            <Chip value="other">Otros</Chip>
-          </Chips>
-
           <div>
           {
-            // @ts-ignore
+            typeof inprod_orders !== 'undefined' && typeof etapas !== 'undefined' && 
             inprod_orders.map(order => { 
+              console.log(order);
               return (
                 <Card className={` rounded-md m-5 p-0 border-2 border-gray-700`} key={`in-prod-${order.id}`}>
                   <div className=" h-full animate-pulse flex flex-col">
-                    <div className=" grid grid-cols-[20%_80%] font-semibold justify-center items-center h-full w-full">
+                    <div className=" grid grid-cols-[15%_85%] font-semibold justify-center items-center h-full w-full">
 
                       <div 
                         className={` bg-lightBackground text-gray-900 border-r-8 ${prod_state === "stopped" ? "border-r-red-600" : "border-r-blue-600"} text-9xl flex justify-center items-center h-full w-full`}
@@ -84,7 +76,7 @@ const OrdersLayout: React.FunctionComponent<{ enoughWidth: boolean, prod_state: 
                           </div>
                           <div className=" flex flex-col items-center">
                             <div className=" text-gray-400 text-opacity-80">ETAPA ACTUAL</div>
-                            <div>{order.name}</div>
+                            <div>{etapas[order.id_etapa].name}</div>
                           </div>
                           <div className=" flex flex-col items-center">
                             <div className=" text-gray-400 text-opacity-80">CLIENTE</div>
@@ -108,34 +100,134 @@ const OrdersLayout: React.FunctionComponent<{ enoughWidth: boolean, prod_state: 
           }
           </div>
 
+          <Chips variant="filled" multiple onChange={setSelectedChip} value={selectedChip} className=" mb-5">
+            <Chip value="approved">Aprobados</Chip>
+            <Chip value="finished">Terminados</Chip>
+            <Chip value="on-hold">En espera</Chip>
+            <Chip value="other">Otros</Chip>
+          </Chips>
+
           <div className=" max-h-full grow">
-          {
-            selectedChip.indexOf('on-hold') !== -1 &&
-            <div className=" mt-5 pt-2 border-t-2 border-gray-600">
-              {/* @ts-ignore */}
-              <Badge size="lg" color={"yellow"}>En espera: {on_hold_orders.length}</Badge>
-              {/* @ts-ignore */}
-              <OrderInProd enoughWidth={enoughWidth} prod_state={"waiting"} orders={on_hold_orders} etapas={etapas} />
-            </div>
-          }
-          {
-            selectedChip.indexOf('approved') !== -1 &&
-            <div className=" mt-5 pt-2 border-t-2 border-gray-600">
-              {/* @ts-ignore */}
-              <Badge size="lg">Aprobados: {approved_orders.length}</Badge>
-              {/* @ts-ignore */}
-              <OrderInProd  enoughWidth={enoughWidth} prod_state={prod_state} orders={approved_orders} etapas={etapas} />
-            </div>
-          }
-          {
-            selectedChip.indexOf('finished') !== -1 &&
-            <div className=" mt-5 pt-2 border-t-2 border-gray-600">
-              {/* @ts-ignore */}
-              <Badge size="lg" color={"green"}>Terminados: {finished_orders.length}</Badge>
-              {/* @ts-ignore */}
-              <OrderInProd enoughWidth={enoughWidth} prod_state={"waiting"} orders={finished_orders} etapas={etapas} />
-            </div>
-          }
+            <table
+              className=' w-full text-sm text-left text-gray-500 dark:text-gray-500'
+            >
+              <thead
+                className=' text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400'
+              >
+                <tr>
+                  <th className=' px-6 py-3'>Pedido</th>
+                  <th className=' px-6 py-3'>Metros</th>
+                  <th className=' px-6 py-3'>Avance</th>
+                </tr>
+              </thead>
+              <tbody>
+                {
+                  selectedChip.indexOf('on-hold') !== -1 && typeof on_hold_orders !== 'undefined' && on_hold_orders?.length > 0 &&
+                  <tr
+                    className=' border-b dark:bg-gray-800 dark:border-gray-700 odd:bg-white even:bg-gray-50 odd:dark:bg-gray-800 even:dark:bg-gray-700'
+                  >
+                    <td className=' px-6 py-4 font-medium text-yellow-300 dark:text-white whitespace-nowrap' colSpan={3}>
+                      <div className="justify-center items-center flex flex-row">
+                        EN ESPERA
+                      </div>
+                    </td>
+                  </tr>
+                }
+                {
+                  selectedChip.indexOf('on-hold') !== -1 && typeof on_hold_orders !== 'undefined' && on_hold_orders?.length > 0 &&
+                  on_hold_orders.map(order => {
+
+                    return (
+                      <tr
+                        className=' border-b dark:bg-gray-800 dark:border-gray-700 odd:bg-white even:bg-gray-50 odd:dark:bg-gray-800 even:dark:bg-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600'
+                      >
+                        <td className=' px-6 py-4 font-medium text-gray-50 dark:text-white whitespace-nowrap'>{`#${order.id}`}</td>
+                        <td className=' px-6 py-4 font-medium text-gray-50 dark:text-white whitespace-nowrap'>{`${order.vueltas}`}</td>
+                        <td 
+                          className=' px-6 py-4 font-medium text-gray-50 dark:text-white whitespace-nowrap justify-center items-center flex flex-col'
+                        >
+                          <ProductionTimeline 
+                            etapa={order.id_etapa}
+                            etapas={etapas}
+                            prod_state={prod_state}
+                            enoughWidth={enoughWidth} 
+                          ></ProductionTimeline>
+                          <div className=" mt-4">
+                            <ApproveOrderBtn order_id={order.id} prod_state={prod_state} />
+                          </div>
+                        </td>
+                      </tr>
+                    )
+                  })
+                }
+                {
+                  selectedChip.indexOf('approved') !== -1 && typeof approved_orders !== 'undefined' && approved_orders?.length > 0 &&
+                  <tr
+                    className=' border-b dark:bg-gray-800 dark:border-gray-700 odd:bg-white even:bg-gray-50 odd:dark:bg-gray-800 even:dark:bg-gray-700'
+                  >
+                    <td className=' px-6 py-4 font-medium text-blue-400 dark:text-white whitespace-nowrap' colSpan={3}>
+                      <div className="justify-center items-center flex flex-row">
+                        APROBADOS
+                      </div>
+                    </td>
+                  </tr>
+                }
+                {
+                  selectedChip.indexOf('approved') !== -1 && typeof approved_orders !== 'undefined' && approved_orders?.length > 0 &&
+                  approved_orders.map(order => {
+                    return (
+                      <tr
+                        className=' border-b dark:bg-gray-800 dark:border-gray-700 odd:bg-white even:bg-gray-50 odd:dark:bg-gray-800 even:dark:bg-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600'
+                      >
+                        <td className=' px-6 py-4 font-medium text-gray-50 dark:text-white whitespace-nowrap'>{`#${order.id}`}</td>
+                        <td className=' px-6 py-4 font-medium text-gray-50 dark:text-white whitespace-nowrap'>{`${order.vueltas}`}</td>
+                        <td className=' px-6 py-4 font-medium text-gray-50 dark:text-white whitespace-nowrap'>
+                          <ProductionTimeline 
+                            etapa={order.id_etapa}
+                            etapas={etapas}
+                            prod_state={"in_prod"}
+                            enoughWidth={enoughWidth} 
+                          ></ProductionTimeline>  
+                        </td>
+                      </tr>
+                    )
+                  })
+                }
+                {
+                  selectedChip.indexOf('finished') !== -1 && typeof finished_orders !== 'undefined' && finished_orders?.length > 0 &&
+                  <tr
+                    className=' border-b dark:bg-gray-800 dark:border-gray-700 odd:bg-white even:bg-gray-50 odd:dark:bg-gray-800 even:dark:bg-gray-700'
+                  >
+                    <td className=' px-6 py-4 font-medium text-green-400 dark:text-white whitespace-nowrap' colSpan={3}>
+                      <div className="justify-center items-center flex flex-row">
+                        TERMINADOS
+                      </div>
+                    </td>
+                  </tr>
+                }
+                {
+                  selectedChip.indexOf('finished') !== -1 && typeof finished_orders !== 'undefined' && finished_orders?.length > 0 && 
+                  finished_orders.map(order => {
+                    return (
+                      <tr
+                        className=' border-b dark:bg-gray-800 dark:border-gray-700 odd:bg-white even:bg-gray-50 odd:dark:bg-gray-800 even:dark:bg-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600'
+                      >
+                        <td className=' px-6 py-4 font-medium text-gray-50 dark:text-white whitespace-nowrap'>{`#${order.id}`}</td>
+                        <td className=' px-6 py-4 font-medium text-gray-50 dark:text-white whitespace-nowrap'>{`${order.vueltas}`}</td>
+                        <td className=' px-6 py-4 font-medium text-gray-50 dark:text-white whitespace-nowrap'>
+                          <ProductionTimeline 
+                            etapa={order.id_etapa}
+                            etapas={etapas}
+                            prod_state={"waiting"}
+                            enoughWidth={enoughWidth} 
+                          ></ProductionTimeline>  
+                        </td>
+                      </tr>
+                    )
+                  })
+                }
+              </tbody>
+            </table>
           </div>
         </div>
       );
